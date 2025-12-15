ad0bbb78a2c46fb609b5f6e0247a0224660bf23a
aboudHasan <aboudhasan06@gmail.com>
2025-12-14 19:16:20 -0500

    boilerplate

---
+++ server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const port = process.env.PORT || 5000;
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}\nhttp://localhost:${port}`);
+});
