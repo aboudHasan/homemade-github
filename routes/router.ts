3b337aa2da470af2f1b4b4454694ef3692eca267
aboudHasan <aboudhasan06@gmail.com>
2025-12-18 17:30:01 -0500

    fixed routing

--- routes/router.ts
+++ routes/router.ts
@@ -14,6 +14,7 @@ const router: express.Router = express.Router();
 router.post("/login", loginLimit, login);
 router.post("/logout", globalLimit, isAuthenticated, logout);
 router.post("/createProject", createLimit, isAuthenticated, createRepo);
+router.get("/projects/:projectName", viewLimit, viewRepos);
 router.get("/projects/:projectName/*splat", viewLimit, viewRepos);
 
 export default router;
