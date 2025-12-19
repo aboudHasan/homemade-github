9eaac2df7f7c0592dd70c298d0c851e0da5a32e6
aboudHasan <aboudhasan06@gmail.com>
2025-12-19 16:24:27 -0500

    trusted one proxy to keep rate limiting accurate

--- server.ts
+++ server.ts
@@ -9,6 +9,7 @@ const __dirname = path.dirname(__filename);
 
 const port = process.env.PORT || 5000;
 const app: express.Express = express();
+app.set("trust proxy", 1);
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
